c68cf1953bd2ae38362dceae9f290e520f624fb4
michelle perez <mmorales@boomsourcing.com>
2020-11-13 11:49:52 -0700

    fonst added

--- App.js
+++ App.js
@@ -4,24 +4,16 @@ import Navigator from './src/navigations/Navigator'
 import { ThemeProvider } from 'styled-components'
 import theme from './src/theme/LightTheme'
 import { AppLoading } from 'expo'
-import {
-  useFonts,
-  Roboto_100Thin,
-  Roboto_300Light,
-  Roboto_400Regular,
-  Roboto_500Medium,
-  Roboto_700Bold,
-  Roboto_900Black,
-} from '@expo-google-fonts/roboto'
+import { useFonts } from '@use-expo/font'
 
 const App = () => {
   let [fontsLoaded] = useFonts({
-    Roboto_100Thin,
-    Roboto_300Light,
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold,
-    Roboto_900Black,
+    Roboto_100Thin: require('./assets/fonts/Roboto-Thin.ttf'),
+    Roboto_300Light: require('./assets/fonts/Roboto-Light.ttf'),
+    Roboto_400Regular: require('./assets/fonts/Roboto-Regular.ttf'),
+    Roboto_500Medium: require('./assets/fonts/Roboto-Medium.ttf'),
+    Roboto_700Bold: require('./assets/fonts/Roboto-Bold.ttf'),
+    Roboto_900Black: require('./assets/fonts/Roboto-Black.ttf'),
   })
 
   if (!fontsLoaded) return <AppLoading />
