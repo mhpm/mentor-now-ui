cbfd287fde97eb636a47491961fd362efd26a9d5
michelle perez <mmorales@boomsourcing.com>
2020-11-18 19:00:55 -0700

    faceboo api integrated

--- src/screens/auth/PrincipalScreen.jsx
+++ src/screens/auth/PrincipalScreen.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { Button, Container, Text, Box, Wrapper } from '../../components'
 import logoImage from '../../../assets/logoW.png'
 import { AppState } from 'react-native'
+import * as Facebook from 'expo-facebook'
 
 const Logo = styled(Image)`
   width: 200px;
@@ -26,15 +27,42 @@ const PrincipalScreen = ({ navigation }) => {
       console.log('avtive')
     }
   }
+
+  const logInFacebook = async () => {
+    try {
+      await Facebook.initializeAsync({
+        appId: '1007474016423973',
+      })
+      const {
+        type,
+        token,
+        expirationDate,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile'],
+      })
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?fields=id,name,picture.type(large),email&access_token=${token}`
+        )
+        const data = await response.json()
+        console.log('Logged in!', data)
+        navigation.navigate('Main')
+      } else {
+        // type === 'cancel'
+        console.log('login cancel')
+      }
+    } catch ({ message }) {
+      console.log(`Facebook Login Error: ${message}`)
+    }
+  }
   return (
     <Container p={4}>
       <Wrapper>
         <Logo source={logoImage}></Logo>
-        <Button
-          fluid
-          variant="blue"
-          onPress={() => navigation.navigate('Main')}
-        >
+        <Button fluid variant="blue" onPress={logInFacebook}>
           Iniciar con Facebook
         </Button>
         <Button fluid onPress={() => navigation.navigate('Login')}>
