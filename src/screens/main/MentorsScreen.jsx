73cc347452a67db5597a260550f427c4732025c2
michelle perez <mmorales@boomsourcing.com>
2021-02-26 21:17:10 -0700

    stable version

--- src/screens/main/MentorsScreen.jsx
+++ src/screens/main/MentorsScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import useFetch from '../../hooks/useFetch'
-import { Container, Wrapper, MentorCard, SearchBar } from '../../components'
+import { Container, Wrapper, MentorCard, Box, Text } from '../../components'
 import { FlatList } from 'react-native'
 
 const MentorsScreen = () => {
@@ -8,7 +8,11 @@ const MentorsScreen = () => {
 
   return (
     <Container pt="12%" px="10px">
-      <SearchBar />
+      <Box mb={10} p={10} bg="primary" width="100%" borderRadius={7}>
+        <Text mx="auto" fontFamily="black">
+          Mentores Disponibles
+        </Text>
+      </Box>
       <Wrapper mdCol={8} loading={loading}>
         <FlatList
           style={{ width: 100 + '%' }}
